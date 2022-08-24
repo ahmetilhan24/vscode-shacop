a9db8e388f8c1fdfb16d2bdb5a5b4fd1420131aa
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-08-24 21:54:51 +0300

    Teams sharing platform added

--- src/constants/sharing-platforms.ts
+++ src/constants/sharing-platforms.ts
@@ -6,6 +6,10 @@ export const SHARING_PLATFORMS: ISharingPlatform[] = [
     url: "https://wa.me/?text=",
     name: "Whatsapp",
   },
+  {
+    url: "https://teams.microsoft.com/l/chat/0/0?users=ailhan%40hepsiemlak.com",
+    name: "Teams",
+  },
 ];
 
 export const QPPlatforms = (): QuickPickItem[] => {
